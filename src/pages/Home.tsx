53b97a8ce76a2c7821edf337b848bb7a446c121b
Ronald Gutierrez <ronald07alex89@gmail.com>
2026-04-02 21:06:24 -0400

    fix: remove unused ObraviolIcon import to fix tsc -b build

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ObraviolLogo, ObraviolIcon } from '../components/ObraviolLogo';
+import { ObraviolLogo } from '../components/ObraviolLogo';
 import {
   Calculator, FileText, BarChart3, Receipt, GanttChart, CreditCard,
   Shield, Award, BadgeCheck, HardHat, ArrowRight, Check,
